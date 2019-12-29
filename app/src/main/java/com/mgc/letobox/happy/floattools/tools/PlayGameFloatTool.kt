c88f49b38b337cb4ccad5e7cacdf6fbe4ce9f46a
leo <leo@163.com>
2019-12-29 21:14:09 +0800

    缓存游戏下载数据
    Signed-off-by: leo <leo@163.com>

--- app/src/main/java/com/mgc/letobox/happy/floattools/tools/PlayGameFloatTool.kt
+++ app/src/main/java/com/mgc/letobox/happy/floattools/tools/PlayGameFloatTool.kt
@@ -1,8 +1,10 @@
 package com.mgc.letobox.happy.floattools.tools
 
 import android.app.Activity
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
+import com.google.gson.Gson
 import com.ledong.lib.leto.LetoConst
 import com.leto.game.base.login.LoginManager
 import com.leto.game.base.util.BaseAppUtil
@@ -12,6 +14,7 @@ import com.mgc.letobox.happy.floattools.MGCService
 import com.mgc.letobox.happy.floattools.components.PlayGameTaskActivity
 import com.mgc.letobox.happy.model.FloatToolsConfig.Data.Playgametask
 import com.mgc.letobox.happy.model.PlayGameResult
+import com.mgc.letobox.happy.util.LeBoxSpUtil
 import com.mgc.letobox.happy.view.PlayGameView
 import retrofit2.Call
 import retrofit2.Response
@@ -54,18 +57,36 @@ class PlayGameFloatTool(activity: Activity, gameId: String, val palygameConfig: 
     }
 
     private fun playGameIt(activity: Activity) {
+        var isEnter = false;
+        try {
+            var resultJson = LeBoxSpUtil.getString(LoginManager.getUserId(activity));
+            if(!TextUtils.isEmpty(resultJson)){
+                val playgameResult: PlayGameResult? = Gson().fromJson(resultJson,PlayGameResult::class.java)
+                if(playgameResult != null){
+                    isEnter = true
+                    PlayGameTaskActivity.start(activity,playgameResult)
+                }
+            }
+        }catch (e:Exception){
+            e.printStackTrace()
+            isEnter = false
+        }
         val service: MGCService = buildRetrofit().create(MGCService::class.java)
         val playGameResultCall: Call<PlayGameResult> = service.obtainPlayGameResult(toInt(BaseAppUtil.getChannelID(activity)),LoginManager.getUserId(activity), LetoConst.SDK_OPEN_TOKEN)
         try {
             val playgameResultResponse: Response<PlayGameResult>? = playGameResultCall.execute()
             if (playgameResultResponse != null) {
                 val playgameResult: PlayGameResult? = playgameResultResponse.body()
-                activity.runOnUiThread {
-                    if(playgameResult == null){
-                        Toast.makeText(activity, "获取数据失败", Toast.LENGTH_SHORT).show()
-                        return@runOnUiThread
+                val json = Gson().toJson(playgameResult)
+                LeBoxSpUtil.saveString(LoginManager.getUserId(activity),json);
+                if(!isEnter){
+                    activity.runOnUiThread {
+                        if(playgameResult == null){
+                            Toast.makeText(activity, "获取数据失败", Toast.LENGTH_SHORT).show()
+                            return@runOnUiThread
+                        }
+                        PlayGameTaskActivity.start(activity,playgameResult)
                     }
-                    PlayGameTaskActivity.start(activity,playgameResult)
                 }
             }
         } catch (e: IOException) {
